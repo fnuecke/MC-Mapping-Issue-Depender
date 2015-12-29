114f7364a551eab2bab6d85192898d4d8581a37a
Florian Nücke <florian@nuecke.de>
2015-12-29 13:37:27 +0100

    Mod depending on mod with method reference.

---
+++ src/main/java/li/cil/test/ModDepender.java
@@ -0,0 +1,9 @@
+package li.cil.test;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ModDepender.MODID, version = ModDepender.VERSION)
+public class ModDepender {
+    public static final String MODID = "test-mapping-depender";
+    public static final String VERSION = "1.0";
+}
